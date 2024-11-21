448410649a59a972cb5b5717bb485eb7deea4507
jiyea0904 <jiyea.park0904@gmail.com>
2024-11-21 15:43:43 +0900

    💄 Scss -> styled-components 형식으로 스타일 변경
    App.jsx, Header.jsx, Content.jsx 파일 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,37 @@
-import "./App.scss";
+//import "./App.scss";
 import { contents } from "./assets/data/data";
 import Content from "./components/Content";
 import Header from "./components/Header";
+import styled from "styled-components";
+
+// Styled Components
+const Main = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Section = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  padding: 20px 40px;
+`;
+
 
 function App() {
   return (
-    <main>
+    <Main>
       <Header />
-      <section>
-        {contents.map((el) => (
-          <Content key={el.id} content={el} />
-        ))}
-      </section>
-    </main>
+      <Section>
+        {contents.map((el) => {
+            console.log(el.id); // id 값을 확인하여 중복이 있는지 체크
+            return <Content key={el.id} content={el} />;
+          })}
+      </Section>
+    </Main>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,37 @@
+import styled from "styled-components";
+
+const HeaderContainer = styled.header`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+  background-color: black;
+
+  ul {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+    list-style: none;
+
+    li {
+      font-size: 16px;
+      font-weight: 400;
+      color: white;
+    }
+  }
+`;
+
 export default function Header() {
   return (
-    <header className="header-container">
+    <HeaderContainer>
       <h1>OZ코딩스쿨</h1>
-      <ul className="menu-container">
+      <ul>
         <li>로그인</li>
         <li>회원가입</li>
         <li>내클래스</li>
       </ul>
-    </header>
+    </HeaderContainer>
   );
 }
